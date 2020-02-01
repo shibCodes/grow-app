8327c62c42b4896e6aeee200d8529af346124811
Siobhan Willoughby <>
2020-02-01 17:33:11 +1100

    Style updates

--- src/Butt/Butt.js
+++ src/Butt/Butt.js
@@ -6,10 +6,10 @@ const butt = (props) => {
     return (
         //<div className="Person" style={style}>
         <div>
-            <div className="Butt butt-outer-light" style={props.outerLight}>
-                <div className="butt-inside butt-outer-dark" style={props.outerDark}></div>
-                <div className="butt-inside butt-inner-light" style={props.innerLight}></div>
-                <div className="butt-inside butt-inner-dark" style={props.innerDark}>{props.title}</div>
+            <div className="Butt butt-outer-light" style={{ boxShadow: props.outerLight.boxShadow}}>
+                <div className="butt-inside butt-outer-dark" style={{ boxShadow: props.outerDark.boxShadow }}></div>
+                <div className="butt-inside butt-inner-light" style={{ boxShadow: props.innerLight.boxShadow }}></div>
+                <div className="butt-inside butt-inner-dark" style={{ boxShadow: props.innerDark.boxShadow }}>{props.title}</div>
             </div>
             <p>{props.outerLight.boxShadow}</p>
         </div>
